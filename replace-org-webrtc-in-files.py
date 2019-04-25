7bf74615dd99418874b84a12268bd9e4ebd3d717
David Nelson <david.nelson@dialogic.com>
2019-04-25 16:04:05 -0700

    mods to the find-replace script.

--- replace-org-webrtc-in-files.py
+++ replace-org-webrtc-in-files.py
@@ -1,5 +1,12 @@
 import os, fnmatch
 
+find_org_dot = "org.webrtc"
+find_org_slash = "org/webrtc"
+find_org_under = "org_webrtc_"
+replace_buzz_dot = "dialogicbuzz.webrtc"
+replace_buzz_slash = "dialogicbuzz/webrtc"
+replace_buzz_under = "dialogicbuzz_webrtc_"
+
 def findReplace(directory, find, replace, filePattern):
     for path, dirs, files in os.walk(os.path.abspath(directory)):
         for filename in fnmatch.filter(files, filePattern):
@@ -13,49 +20,76 @@ def findReplace(directory, find, replace, filePattern):
 
 # modules/audio_device
 
-findReplace("android/webrtc/src/modules/audio_device", "org.webrtc", "dialogicbuzz.webrtc", "*.gn")
+mypath = "android/webrtc/src/modules/audio_device"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.gn")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.gn")
 
-findReplace("android/webrtc/src/modules/audio_device", "org/webrtc", "dialogicbuzz/webrtc", "*.gn")
+mypath = "android/webrtc/src/modules/audio_device/android"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.h")
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.cc")
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.java")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.h")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.cc")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.java")
 
-findReplace("android/webrtc/src/modules/audio_device/android/java/src/dialogicbuzz/webrtc/voiceengine", "org.webrtc", "dialogicbuzz.webrtc", "*.java")
+# modules/audio_coding
 
-findReplace("android/webrtc/src/modules/audio_device/android/java/src/dialogicbuzz/webrtc/voiceengine", "org/webrtc", "dialogicbuzz/webrtc", "*.java")
+mypath = "android/webrtc/src/modules/audio_coding/audio_network_adapter"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.proto")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.proto")
+
+# modules/utility
+
+mypath = "android/webrtc/src/modules/utility"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.h")
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.cc")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.h")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.cc")
 
 # rtc_base
 
-findReplace("android/webrtc/src/rtc_base", "org.webrtc", "dialogicbuzz.webrtc", "*.gn")
+mypath = "android/webrtc/src/rtc_base"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.gn")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.gn")
 
-findReplace("android/webrtc/src/rtc_base", "org/webrtc", "dialogicbuzz/webrtc", "*.gn")
-
-findReplace("android/webrtc/src/rtc_base/java/src/dialogicbuzz/webrtc", "org.webrtc", "dialogicbuzz.webrtc", "*.java")
-
-findReplace("android/webrtc/src/rtc_base/java/src/dialogicbuzz/webrtc", "org/webrtc", "dialogicbuzz/webrtc", "*.java")
+mypath = "android/webrtc/src/rtc_base/java"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.java")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.java")
 
 # sdk/android
 
-findReplace("android/webrtc/src/sdk/android", "org.webrtc", "dialogicbuzz.webrtc", "AndroidManifest.xml")
+mypath = "android/webrtc/src/sdk/android"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "AndroidManifest.xml")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "AndroidManifest.xml")
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.gn")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.gn")
 
-findReplace("android/webrtc/src/sdk/android", "org/webrtc", "dialogicbuzz/webrtc", "AndroidManifest.xml")
+mypath = "android/webrtc/src/sdk/android/api"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.java")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.java")
 
-findReplace("android/webrtc/src/sdk/android", "org.webrtc", "dialogicbuzz.webrtc", "*.gn")
+mypath = "android/webrtc/src/sdk/android/src/java"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.java")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.java")
 
-findReplace("android/webrtc/src/sdk/android", "org/webrtc", "dialogicbuzz/webrtc", "*.gn")
-
-findReplace("android/webrtc/src/sdk/android/api/dialogicbuzz/webrtc", "org.webrtc", "dialogicbuzz.webrtc", "*.java")
-
-findReplace("android/webrtc/src/sdk/android/api/dialogicbuzz/webrtc", "org/webrtc", "dialogicbuzz/webrtc", "*.java")
-
-findReplace("android/webrtc/src/sdk/android/src/java/dialogicbuzz/webrtc", "org.webrtc", "dialogicbuzz.webrtc", "*.java")
-
-findReplace("android/webrtc/src/sdk/android/src/java/dialogicbuzz/webrtc", "org/webrtc", "dialogicbuzz/webrtc", "*.java")
-
-findReplace("android/webrtc/src/sdk/android/src/jni", "org_webrtc_", "dialogicbuzz_webrtc_", "*.h")
-
-findReplace("android/webrtc/src/sdk/android/src/jni", "org_webrtc_", "dialogicbuzz_webrtc_", "*.cc")
+mypath = "android/webrtc/src/sdk/android/src/jni"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.h")
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.cc")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.h")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.cc")
+findReplace(mypath, find_org_under, replace_buzz_under, "*.h")
+findReplace(mypath, find_org_under, replace_buzz_under, "*.cc")
 
 # examples/androidapp
 
-findReplace("android/webrtc/src/examples/androidapp/src/org/appspot/apprtc", "org.webrtc", "dialogicbuzz.webrtc", "*.java")
+mypath = "android/webrtc/src/examples/androidapp/src"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.java")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.java")
+
+mypath = "android/webrtc/src/examples/androidapp/res/layout"
+findReplace(mypath, find_org_dot, replace_buzz_dot, "*.xml")
+findReplace(mypath, find_org_slash, replace_buzz_slash, "*.xml")
+
 
 
 
